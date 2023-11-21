7a48eceed28dd9b6cc96b4dd256ccd9bcb946c65
Sevgi <64860005+GIGI-bit@users.noreply.github.com>
2023-11-22 02:57:18 +0400

    Add files via upload

---
+++ DocumentProj/DocumentProj/Program.cs
@@ -0,0 +1,34 @@
+﻿using DocumentProj;
+
+while (true)
+{
+
+    Console.WriteLine("Enter data: ");
+
+    string userData = Console.ReadLine();
+
+    if (userData == "basic")
+    {
+        DocumentProgram documentProgram = new DocumentProgram();
+        documentProgram.OpenDocument();
+        documentProgram.SaveDocument();
+        documentProgram.EditDocument();
+    }
+    else if (userData == "pro")
+    {
+        DocumentProgram proDocumentProgram = new ProDocumentProgram();
+        proDocumentProgram.OpenDocument();
+        proDocumentProgram.SaveDocument();
+        proDocumentProgram.EditDocument();
+    }
+    else if (userData == "expert")
+    {
+        DocumentProgram expertDocument = new ExpertDocument();
+        expertDocument.OpenDocument();
+        expertDocument.SaveDocument();
+        expertDocument.EditDocument();
+    }
+
+
+
+}
